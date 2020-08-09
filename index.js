fa6069449ea69b5d561be4555f1d7d33670d5139
ntello <team@unarix.com.ar>
2020-08-09 19:30:03 -0300

    key secret

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = '';
-
 const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Bot Bardeador running!/h1>');
+  res.end('<h1>Bot Bardeador running!</h1>');
 });
+
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  accessKey: process.env.KEY,
+});
+
+const token = s3.accessKey;
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
